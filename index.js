41e8b48e689609ca14427a3df7cd529897813930
Kraken536 <abd.aa536@gmail.com>
2023-02-04 23:33:57 +0300

    I kept working on the js file only.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 //Now Let's configure the javascript:
 
 const selectBox = document.querySelector(".select-box");
+const playboard = document.querySelector(".play-board");
+const resultBox = document.querySelector(".result-box");
