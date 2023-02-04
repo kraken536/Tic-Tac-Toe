d004b2cc1381b52ee6c3501422d2a417e081f090
Kraken536 <abd.aa536@gmail.com>
2023-02-04 23:53:04 +0300

    The elements were selected in the js file.

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 const selectBox = document.querySelector(".select-box");
 const playboard = document.querySelector(".play-board");
+const wonText = document.querySelector(".won-text");
 const resultBox = document.querySelector(".result-box");
