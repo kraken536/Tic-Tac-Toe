11beb4a4e85c8201532e1b2b2dc7e369955604f7
Kraken536 <abd.aa536@gmail.com>
2023-02-05 22:01:47 +0300

    X and O players button effects were all configured.

--- index.js
+++ index.js
@@ -7,29 +7,30 @@ const playboard = document.querySelector(".play-board");
 const wonText = document.querySelector(".won-text");
 const resultBox = document.querySelector(".result-box");
 const allBox = document.querySelectorAll("section span");
-const players = document.querySelectorAll(".players");
+const players = document.querySelector(".players");
 
 window.onload = ()=> { //Once the window is loaded...onload works
+
+    for(let i = 0; i < allBox.length; i++){
+        allBox[i].setAttribute("onclick", "clickedBox(this)");
+    };
+
     selectXBtn.addEventListener("click", ()=>{
         selectBox.classList.add("hide");
         playboard.classList.add("show");
+        players.setAttribute("class","players activeX");
     });
 
     selectOBtn.addEventListener("click", ()=>{
         selectBox.classList.add("hide");
         playboard.classList.add("show");
-        players.setAttribute("class", "players active");
+        players.setAttribute("class","players activeO");
     });
-
-    
-    for(let i = 0; i < allBox.length; i++){
-        allBox[i].setAttribute("onclick", "clickedBox(this)");
-    }
 }
 
 let playerXicon = "fas fa-times"; //class name of fontawesome cross icon.
 let playerOicon = "far fa-circle"; //class name of fontawesome circle icon.
 
 function clickedBox(element){
-
+    
 }
